31ec409bbde4425d8f2203ccfffa6ed1652598e7
Halo-Sec <107488713+Halo-Sec@users.noreply.github.com>
2023-06-03 12:18:43 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -25,13 +25,13 @@ pipeline {
             stages{
               stage('Build') {
                 steps {
-                  sh 'mvn clean package'
+                  mvn 'clean package'
                 }
               }
               
               stage('Test') {
                 steps {
-                  sh 'mvn test'
+                  mvn 'test'
                 }
               }
             }
