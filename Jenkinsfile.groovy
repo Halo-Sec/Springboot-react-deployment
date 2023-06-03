5f4529eefa5e09102bc8064d01b04d2c3b6652f2
Halo-Sec <107488713+Halo-Sec@users.noreply.github.com>
2023-06-03 12:31:21 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -55,9 +55,20 @@ pipeline {
         script {
           docker.image('chriscamicas/awscli-awsebcli').inside {
             withAWS(credentials: 'aws-credentials') {
-              stage('prepare environment') {
-                sh "eb init ${AWS_EB_APP_NAME} --keyname 'Spring' --platform 'Docker Running on 64bit Amazon Linux 2' --region ${AWS_REGION}"
-                sh "eb create ${AWS_EB_ENV_NAME} --cname-prefix ${AWS_EB_ENV_NAME} --instance-type t2.micro --platform 'Docker Running on 64bit Amazon Linux 2'"
+              stages{
+                stage('prepare environment') {
+                  steps {
+                    sh "eb init ${AWS_EB_APP_NAME} --keyname 'Spring' --platform 'Docker Running on 64bit Amazon Linux 2' --region ${AWS_REGION}"
+                    sh "eb create ${AWS_EB_ENV_NAME} --cname-prefix ${AWS_EB_ENV_NAME} --instance-type t2.micro --platform 'Docker Running on 64bit Amazon Linux 2'"
+                  }
+                } 
+                
+                stage ('deployment'){
+                  steps {
+                    sh 'eb deploy --label "version ${BUILD_NUMBER}"'
+                    sh 'eb status'
+                  }
+                }
               }
             }
           }
