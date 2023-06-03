8c6d7b04dbeb204a9170b666f726a3592d903a47
Halo-Sec <107488713+Halo-Sec@users.noreply.github.com>
2023-06-03 12:04:22 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -41,9 +41,9 @@ pipeline {
     stage('Get Container ID') {
       steps {
         script {
-          def containerId = sh(returnStdout: true, script: 'docker ps -q --filter ancestor=maven:latest').trim()
-          def jar_file = sh(returnStdout: true, script: "docker exec ${containerId} find . -type f -name \"react-and-spring-data-rest-0.0.1-SNAPSHOT.jar\"").trim()
-          sh "docker cp ${containerId}:${jar_file} ./"
+          def containerId = sh(script: 'docker ps -q --filter ancestor=maven:latest', returnStdout: true).trim()
+          def jarFile = sh(script: "docker exec ${containerId} find . -type f -name 'react-and-spring-data-rest-0.0.1-SNAPSHOT.jar'", returnStdout: true).trim()
+          sh "docker cp ${containerId}:${jarFile} ./"
         }
       }
     }
@@ -54,8 +54,8 @@ pipeline {
           docker.image('chriscamicas/awscli-awsebcli').inside {
             withAWS(credentials: 'aws-credentials') {
               stage('prepare environment') {
-                sh "eb init ${AWS_EB_APP_NAME} --keyname \"Spring\" --platform \"Docker Running on 64bit Amazon Linux 2\" --region ${AWS_REGION}"
-                sh "eb create ${AWS_EB_ENV_NAME} --cname-prefix ${AWS_EB_ENV_NAME} --instance-type t2.micro --platform \"Docker Running on 64bit Amazon Linux 2\""
+                sh "eb init ${AWS_EB_APP_NAME} --keyname 'Spring' --platform 'Docker Running on 64bit Amazon Linux 2' --region ${AWS_REGION}"
+                sh "eb create ${AWS_EB_ENV_NAME} --cname-prefix ${AWS_EB_ENV_NAME} --instance-type t2.micro --platform 'Docker Running on 64bit Amazon Linux 2'"
               }
             }
           }
