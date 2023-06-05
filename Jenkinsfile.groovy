d696bf42916a369d5987dde6d5f00b7db2ed0b50
Halo-Sec <107488713+Halo-Sec@users.noreply.github.com>
2023-06-05 15:44:54 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,36 +6,32 @@ pipeline {
     AWS_EB_ENV_NAME = 'springbootapp-env'
     AWS_REGION = 'us-east-1'
   }
+
   triggers {
-    pollSCM ('*/1 * * * *')
+    pollSCM('*/1 * * * *')
   }
 
-  stages{
-    stage ('checkout') {
-      steps{
+  stages {
+    stage('Checkout') {
+      steps {
         checkout scm
       }
     }
-    
-    stage ('Build and test') {
+
+    stage('Build and Test') {
       steps {
         sh 'mvn clean package'
         sh 'mvn test'
       }
     }
-  
 
-    stage ('Deploy') {
+    stage('Deploy') {
       steps {
-        withAWS(credentials: 'aws-credentials'){
-          stage ('prepare environment'){
-            sh 'eb init ${AWS_EB_APP_NAME} --keyname "Spring" --platform "Docker Running on 64bit Amazon Linux 2" --region $(AWS_REGION)'
-            sh 'eb create ${AWS_EB_ENV_NAME} --cname-prefix ${AWS_EB_ENV_NAME} --instance-type t2.micro --platform "Docker Running on 64bit Amazon Linux 2"'
-          }
-          stage ('deployment'){
-            sh 'eb deploy --label "version ${BUILD_NUMBER}"'
-            sh 'eb status'
-          }
+        withAWS(credentials: 'aws-credentials') {
+          sh "eb init ${AWS_EB_APP_NAME} --keyname 'Spring' --platform 'Docker Running on 64bit Amazon Linux 2' --region ${AWS_REGION}"
+          sh "eb create ${AWS_EB_ENV_NAME} --cname-prefix ${AWS_EB_ENV_NAME} --instance-type t2.micro --platform 'Docker Running on 64bit Amazon Linux 2'"
+          sh "eb deploy --label 'version ${BUILD_NUMBER}'"
+          sh 'eb status'
         }
       }
     }
