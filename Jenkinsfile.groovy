4ab9384a8d8c55c71801f43508da9e3d08d83e98
Halo-Sec <107488713+Halo-Sec@users.noreply.github.com>
2023-06-05 15:39:00 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,47 +19,22 @@ pipeline {
     
     stage ('Build and test') {
       steps {
-        script {
-          docker.image ('maven').inside('-v /home/ec2-user/.m2/repository:/root/.m2/repository') {
-            stage ('Build') {
-              sh 'mvn clean package'
-            }
-          
-            stage ('Test') {
-              sh 'mvn test'
-            }    
-          }
-        }
+        sh 'mvn clean package'
+        sh 'mvn test'
       }
     }
   
-   
-
-    stage ('Get Container ID') {
-      steps {
-        script {
-          def containerId = sh(returnStdout: true, script: 'docker ps -q --filter ancestor=maven:latest').trim()
-          def jar_file = sh(returnStdout: true, script: 'docker exec ${containerId} find . -type f -name "react-and-spring-data-rest-0.0.1-SNAPSHOT.jar"').trim ()
-          sh 'docker cp ${containerId}:${jar_file} ./'
-        }        
-      }
-    }
 
     stage ('Deploy') {
       steps {
-        script {
-          docker.image ('chriscamicas/awscli-awsebcli'). inside{
-            withAWS(credentials: 'aws-credentials'){
-              stage ('prepare environment'){
-                sh 'eb init ${AWS_EB_APP_NAME} --keyname "Spring" --platform "Docker Running on 64bit Amazon Linux 2" --region $(AWS_REGION)'
-                sh 'eb create ${AWS_EB_ENV_NAME} --cname-prefix ${AWS_EB_ENV_NAME} --instance-type t2.micro --platform "Docker Running on 64bit Amazon Linux 2"'
-              }
-              
-              stage ('deployment'){
-                sh 'eb deploy --label "version ${BUILD_NUMBER}"'
-                sh 'eb status'
-              }
-            }
+        withAWS(credentials: 'aws-credentials'){
+          stage ('prepare environment'){
+            sh 'eb init ${AWS_EB_APP_NAME} --keyname "Spring" --platform "Docker Running on 64bit Amazon Linux 2" --region $(AWS_REGION)'
+            sh 'eb create ${AWS_EB_ENV_NAME} --cname-prefix ${AWS_EB_ENV_NAME} --instance-type t2.micro --platform "Docker Running on 64bit Amazon Linux 2"'
+          }
+          stage ('deployment'){
+            sh 'eb deploy --label "version ${BUILD_NUMBER}"'
+            sh 'eb status'
           }
         }
       }
