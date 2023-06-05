6b8ee17df9b64a0e5772404bb75f1c2a84eef7bd
Halo-Sec <107488713+Halo-Sec@users.noreply.github.com>
2023-06-05 11:38:51 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,69 +2,64 @@ pipeline {
   agent any
 
   environment {
+    AWS_KEY_ID = credentials ('')
+    AWS_SECRET_KEY = credentials ('')
     AWS_EB_APP_NAME = 'springbootapp'
     AWS_EB_ENV_NAME = 'springbootapp-env'
     AWS_REGION = 'us-east-1'
   }
-
   triggers {
-    pollSCM('*/1 * * * *')
+    pollSCM ('*/1 * * * *')
   }
 
-  stages {
-    stage('checkout') {
-      steps {
+  stages{
+    stage ('checkout') {
+      steps{
         checkout scm
       }
     }
-
-    stage('Build and test') {
+    
+    stage ('Build and Test') {
       steps {
         script {
-          docker.image('maven').inside {
-            stage('Build') {
-              steps {
-                sh 'mvn clean package'
-              }
+          docker.image ('maven').inside{
+            stage ('Build') {
+              sh 'mvn clean package'
             }
-
-            stage('Test') {
-              steps {
-                sh 'mvn test'
-              }
-            }
+          
+            stage ('Test') {
+              sh 'mvn test'
+            }    
           }
         }
       }
     }
+  
+   
 
-    stage('Get Container ID') {
+    stage ('Get Container ID') {
       steps {
         script {
-          def containerId = sh(script: 'docker ps -q --filter ancestor=maven:latest', returnStdout: true).trim()
-          def jarFile = sh(script: "docker exec ${containerId} find . -type f -name 'react-and-spring-data-rest-0.0.1-SNAPSHOT.jar'", returnStdout: true).trim()
-          sh "docker cp ${containerId}:${jarFile} ./"
-        }
+          def containerId = sh(returnStdout: true, script: 'docker ps -q --filter ancestor=maven:latest').trim()
+          def jar_file = sh(returnStdout: true, script: 'docker exec ${containerId} find . -type f -name "react-and-spring-data-rest-0.0.1-SNAPSHOT.jar"').trim ()
+          sh 'docker cp ${containerId}:${jar_file} ./'
+        }        
       }
     }
 
-    stage('Deploy') {
+    stage ('Deploy') {
       steps {
         script {
-          docker.image('chriscamicas/awscli-awsebcli').inside {
-            withAWS(credentials: 'aws-credentials') {
-              stage('prepare environment') {
-                steps {
-                  sh "eb init ${AWS_EB_APP_NAME} --keyname 'Spring' --platform 'Docker Running on 64bit Amazon Linux 2' --region ${AWS_REGION}"
-                  sh "eb create ${AWS_EB_ENV_NAME} --cname-prefix ${AWS_EB_ENV_NAME} --instance-type t2.micro --platform 'Docker Running on 64bit Amazon Linux 2'"
-                }
+          docker.image ('chriscamicas/awscli-awsebcli'). inside{
+            withAWS(credentials: 'aws-credentials'){
+              stage ('prepare environment'){
+                sh 'eb init ${AWS_EB_APP_NAME} --keyname "Spring" --platform "Docker Running on 64bit Amazon Linux 2" --region $(AWS_REGION)'
+                sh 'eb create ${AWS_EB_ENV_NAME} --cname-prefix ${AWS_EB_ENV_NAME} --instance-type t2.micro --platform "Docker Running on 64bit Amazon Linux 2"'
               }
-
-              stage('deployment') {
-                steps {
-                  sh 'eb deploy --label "version ${BUILD_NUMBER}"'
-                  sh 'eb status'
-                }
+              
+              stage ('deployment'){
+                sh 'eb deploy --label "version ${BUILD_NUMBER}"'
+                sh 'eb status'
               }
             }
           }
